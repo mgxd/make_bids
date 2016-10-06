8e1ce6af5abef053a002311cf5e564aa66090c60
mathiasg <mathiasg@mit.edu>
2016-10-06 17:42:19 -0400

    add: simple setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import sys
+from distutils.core import setup
+
+setup(name='makebids',
+      version='dev',
+      author='Mathias Goncalves',
+      author_email='mathiasg@mit.edu',
+      package_dir = {'': 'bin'})
