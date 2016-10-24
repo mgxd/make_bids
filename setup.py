05d49e735ca10aed86cbc9a1300889b470099900
mathiasg <mathiasg@mit.edu>
2016-10-24 16:39:07 -0400

    test: depend links

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(name='makebids',
       author='Mathias Goncalves',
       author_email='mathiasg@mit.edu',
       packages=['makebids'],
-      install_requires = ['pydicom'],
-      dependency_links=['https://github.com/INCF/pybids/archive/158dac2062dc6b5a4ab2f92090108eedc3387575.zip'],
+      install_requires = ['pydicom',
+                          'pybids'],
+      dependency_links=['https://github.com/INCF/pybids/tree/master/bids#egg'],
       entry_points={'console_scripts': 
                  ['makebids=makebids.makebids:main']})
